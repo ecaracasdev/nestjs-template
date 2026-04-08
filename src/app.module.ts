7e56e330e8812887a4d0ca4620bc993ccf2194cf
elias.caracas <elias.caracas@comafi.com.ar>
2026-04-08 18:18:50 -0300

    add joi validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,42 @@
 import { Module } from '@nestjs/common';
 import { TodoListsModule } from './todo_lists/todo_lists.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TodoList } from './todo_lists/todo_list.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HealthController } from './health/health.controller';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { LoggerModule } from './common/logger/logger.module';
+import * as Joi from 'joi';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [databaseConfig],
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().default(5432), // Si no viene, usa 5432
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+      }),
     }),
     TodoListsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [TodoList],
-      synchronize: true,
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions =>
+        ({
+          type: 'postgres',
+          ...configService.get('database'),
+          entities: [TodoList],
+          synchronize: true,
+          logging: true,
+        }) as TypeOrmModuleOptions, // <--- El "as" es el truco para que TS se relaje
     }),
     LoggerModule,
   ],
